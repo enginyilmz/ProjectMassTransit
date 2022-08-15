32a14b12e623ee6046245e48f7087fb7a53f8333
Engin Yılmaz <engin@ileriisler.com>
2022-08-15 22:35:22 +0100

    first commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using EventContracts;
+using MassTransit;
+
+var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+{
+    cfg.Host("amqp://admin:password@192.168.1.2:5672", conf =>
+    {
+        conf.Username("admin");
+        conf.Password("password");
+    });
+
+    cfg.ReceiveEndpoint("event-listener", e =>
+    {
+        e.Consumer<EventConsumer>();
+    });
+});
+
+var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+await busControl.StartAsync(source.Token);
+try
+{
+    Console.WriteLine("Press enter to exit");
+
+    await Task.Run(() => Console.ReadLine());
+}
+finally
+{
+    await busControl.StopAsync();
+}
+
+class EventConsumer :
+            IConsumer<ValueEntered>
+{
+    public async Task Consume(ConsumeContext<ValueEntered> context)
+    {
+        Console.WriteLine("Value: {0}", context.Message.Value);
+    }
+}
+
+namespace EventContracts
+{
+    public interface ValueEntered
+    {
+        string Value { get; }
+    }
+}
+
---
+++ Producer/Program.cs
@@ -0,0 +1,46 @@
+﻿using EventContracts;
+using MassTransit;
+
+var busControl = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host("amqp://admin:password@192.168.1.2:5672", conf =>
+    {
+        conf.Username("admin");
+        conf.Password("password");
+    });
+});
+var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+await busControl.StartAsync(source.Token);
+try
+{
+    while (true)
+    {
+        string value = await Task.Run(() =>
+        {
+            Console.WriteLine("Enter message (or quit to exit)");
+            Console.Write("> ");
+            return Console.ReadLine();
+        });
+
+        if ("quit".Equals(value, StringComparison.OrdinalIgnoreCase))
+            break;
+
+        await busControl.Publish<ValueEntered>(new
+        {
+            Value = value
+        });
+    }
+}
+finally
+{
+    await busControl.StopAsync();
+}
+
+namespace EventContracts
+{
+    public interface ValueEntered
+    {
+        string Value { get; }
+    }
+}
+
